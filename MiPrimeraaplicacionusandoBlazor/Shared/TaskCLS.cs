a6adbf4ec0d45c8e6f14f1b928a812bd5707bfab
ertugrul yilmaz <bestcodder@outlook.com>
2021-04-15 23:08:33 +0300

    bind propertysi ile ılgılı calısmalar (2_13)

---
+++ MiPrimeraaplicacionusandoBlazor/Shared/TaskCLS.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiPrimeraaplicacionusandoBlazor.Shared
+{
+    public class TaskCLS
+    {
+        public string DayOfWeek { get; set; }
+        public string Task { get; set; }
+        public bool Perform { get; set; }
+        public DateTime TaskDate { get; set; }
+    }
+}
