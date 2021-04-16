854a44495af87d4e4b2a9d8587308cb32add1668
ertugrul yilmaz <bestcodder@outlook.com>
2021-04-16 22:39:14 +0300

    Book Type Lİst genel bır lısteleme componente tasındı - 19

--- MiPrimeraaplicacionusandoBlazor/Server/Controllers/BookTypeController.cs
+++ MiPrimeraaplicacionusandoBlazor/Server/Controllers/BookTypeController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MiPrimeraaplicacionusandoBlazor.Server.Models;
+using MiPrimeraaplicacionusandoBlazor.Shared;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MiPrimeraaplicacionusandoBlazor.Server.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class BookTypeController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet("GetBookTypeList")]
+        public List<BookTypeCLS> Get()
+        {
+            List<BookTypeCLS> bookList = new List<BookTypeCLS>();
+            using (BlazorLibraryDBContext db = new BlazorLibraryDBContext())
+            {
+                bookList = (from bookType in db.BookTypes
+                           where bookType.Active == 1
+                           select new BookTypeCLS
+                           {
+                               Id = bookType.Id,
+                               BookTypeName = bookType.BookTypeName,
+                               Description = bookType.Description
+                           }).ToList();
+            }
+            return bookList;
+        }
+
     }
 }
