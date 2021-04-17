1d9dd123b81961dac57d78e0422b5a912aa0fe02
ertugrul yilmaz <bestcodder@outlook.com>
2021-04-17 18:39:12 +0300

    Yazar Listesi Sayfasında ülkelere gıre ama ıslemlerı yapıldı - 24

--- MiPrimeraaplicacionusandoBlazor/Server/Controllers/AuthorController.cs
+++ MiPrimeraaplicacionusandoBlazor/Server/Controllers/AuthorController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MiPrimeraaplicacionusandoBlazor.Server.Models;
 using MiPrimeraaplicacionusandoBlazor.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+
 namespace MiPrimeraaplicacionusandoBlazor.Server.Controllers
 {
     [Route("api/[controller]")]
@@ -20,16 +24,63 @@ namespace MiPrimeraaplicacionusandoBlazor.Server.Controllers
                               on author.CountryId equals country.Id
                               join gender in db.SexTypes
                               on author.SexTypeId equals gender.Id
+                              where author.Active == 1
                               select new AuthorCLS
                               {
-                                 Id = author.Id,
-                                 Fullname = author.Username,
-                                 CountryName = country.CountryName,
-                                 SexTypeName =gender.SexTypeName,
-                                 Description = author.Description
+                                  Id = author.Id,
+                                  Fullname = author.Username,
+                                  CountryName = country.CountryName,
+                                  SexTypeName = gender.SexTypeName,
+                                  Description = author.Description
                               }).ToList();
             }
             return listAuthor;
         }
+
+        [HttpGet("listcountry")]
+        public List<CountryCLS> CountryList()
+        {
+            List<CountryCLS> listCountry = new List<CountryCLS>();
+            using (var db = new BlazorLibraryDBContext())
+            {
+                listCountry = (from country in db.Countries
+                               where country.Active == 1
+                               select new CountryCLS
+                               {
+                                   Id = country.Id,
+                                   CountryName = country.CountryName
+                               }).ToList();
+            }
+            return listCountry;
+        }
+
+
+        [HttpGet("filterauthor/{data?}")]
+        public List<AuthorCLS> FilterAuthor(string data)
+        {
+            List<AuthorCLS> listAuthor = new List<AuthorCLS>();
+            using (var db = new BlazorLibraryDBContext())
+            {
+                if (data != null || data == "--Selected One Item--")
+                {
+                    listAuthor = (from author in db.Authors
+                                  join country in db.Countries
+                                  on author.CountryId equals country.Id
+                                  join gender in db.SexTypes
+                                  on author.SexTypeId equals gender.Id
+                                  where author.Active == 1 && author.Id == Convert.ToInt32(data)
+                                  select new AuthorCLS
+                                  {
+                                      Id = author.Id,
+                                      Fullname = author.Username,
+                                      CountryName = country.CountryName,
+                                      SexTypeName = gender.SexTypeName,
+                                      Description = author.Description
+                                  }).ToList();
+                }
+                return listAuthor;
+            }
+        }
+
     }
 }
